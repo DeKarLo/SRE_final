52e5e12c2a66742915750b91f4f5db1e8b3e01ca
DeKarLo <karen.ananyan2017@mail.ru>
2024-06-07 22:43:20 +0500

    fix py

--- webhook_server.py
+++ webhook_server.py
@@ -5,7 +5,7 @@ class WebhookHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         self.send_response(200)
         self.end_headers()
-        subprocess.run(["/bin/bash", "/path/to/webhook_handler.sh"])
+        subprocess.run(["/bin/bash", "/home/ubuntu/dev/SRE_final/webhook_handler.sh"])
 
 server_address = ('', 8000)
 httpd = HTTPServer(server_address, WebhookHandler)
