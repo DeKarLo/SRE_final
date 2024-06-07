b9ea44dc31b41f96a7e42b5e103e11c2bf3fce85
DeKarLo <karen.ananyan2017@mail.ru>
2024-06-08 03:24:15 +0500

    kapec

--- webhook_handler.sh
+++ webhook_handler.sh
@@ -2,8 +2,14 @@
 
 COMPOSE_FILE_PATH=/home/ubuntu/dev/SRE_final/
 
+# Change to the directory
 cd $COMPOSE_FILE_PATH
-git pull
+
+# Log output
+GIT_LOG=/home/ubuntu/dev/SRE_final/git_pull.log
+docker-compose -f $COMPOSE_FILE_PATH pull >> $GIT_LOG 2>&1
+git pull >> $GIT_LOG 2>&1
+
 # Pull the latest images
 docker-compose pull
 
@@ -11,4 +17,3 @@ docker-compose pull
 docker-compose up -d --remove-orphans
 
 # Optional: Remove old images
-
--- webhook_server.py
+++ webhook_server.py
@@ -1,12 +1,17 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import subprocess
+import logging
+
+logging.basicConfig(filename='/home/ubuntu/dev/SRE_final/webhook_server.log', level=logging.INFO)
 
 class WebhookHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         self.send_response(200)
         self.end_headers()
-        print("Webhook received!")
-        print(subprocess.run(["/bin/bash", "/home/ubuntu/dev/SRE_final/webhook_handler.sh"]))
+        logging.info("Webhook received!")
+        result = subprocess.run(["/bin/bash", "/home/ubuntu/dev/SRE_final/webhook_handler.sh"], capture_output=True, text=True)
+        logging.info(result.stdout)
+        logging.error(result.stderr)
 
 server_address = ('', 8000)
 httpd = HTTPServer(server_address, WebhookHandler)
