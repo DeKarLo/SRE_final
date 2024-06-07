16e86ec1a097632645819ac3d770927f23a57bbd
DeKarLo <karen.ananyan2017@mail.ru>
2024-06-08 03:15:16 +0500

    awdawa

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
 collector = BotAiogramCollector()
 collector.add_bot(bot)
 bot.session.middleware(PrometheusMetricRequestMiddleware())
