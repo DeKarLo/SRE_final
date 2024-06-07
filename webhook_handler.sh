16c80972a07df64581848042fe1def6a3b16a01d
DeKarLo <karen.ananyan2017@mail.ru>
2024-06-08 02:42:11 +0500

    fix

--- webhook_handler.sh
+++ webhook_handler.sh
@@ -2,6 +2,11 @@
 
 COMPOSE_FILE_PATH=/home/ubuntu/dev/SRE_final/docker-compose.yml
 
+# Pull the latest images
 docker-compose -f $COMPOSE_FILE_PATH pull
 
-docker-compose -f $COMPOSE_FILE_PATH up -d
+# Restart the services
+docker-compose -f $COMPOSE_FILE_PATH up -d --remove-orphans
+
+# Optional: Remove old images
+docker image prune -f
