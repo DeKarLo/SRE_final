883e5d54831c06ca718f1684fb5bcfab10b97b4b
DeKarLo <karen.ananyan2017@mail.ru>
2024-06-08 02:59:49 +0500

    робитвй

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ class _Storage(PrometheusMetricStorageMixin, MemoryStorage):
 storage_collector = StorageAiogramCollector()
 storage = _Storage(storage_collector)
 dp = Dispatcher(storage=storage, bot=bot)
-fsefsefsefsef
-logger.info("Bot started!")SYJUGcAQHGWvcHASV Cgh
+
+logger.info("Bot started!")
 
 dp.include_routers(callback_handlers.router, command_handlers.router)
 dp.message.middleware(PrometheusMetricMessageMiddleware())
